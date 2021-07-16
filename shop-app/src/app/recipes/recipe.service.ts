8142c93e9dbb1ea0dfcefcb3f7c1efddd25b0721
AnubhutiRoX <s.anubhuti1987@gmail.com>
2021-07-16 10:58:20 +0530

    recipe service
    by adding service, all listneers are removed which were required for communication across recipedetail and recipelist to identify the recipe data which got selected from the list of recipes.

--- shop-app/src/app/recipes/recipe.service.ts
+++ shop-app/src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
+import {EventEmitter} from '@angular/core';
 import {Recipe} from './recipe.model';
 
 export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+
     private recipes: Recipe[] = [
         new Recipe('A Test Recipe', 'This is a simpl test', 'http://localhost:4200/assets/Recipe.png'),
         new Recipe('A Test Recipe', 'This is a simpl test', 'http://localhost:4200/assets/Recipe.png')
--- shop-app/src/app/recipes/recipes.component.ts
+++ shop-app/src/app/recipes/recipes.component.ts
@@ -11,9 +11,15 @@ import { RecipeService } from './recipe.service';
 export class RecipesComponent implements OnInit {
   @Input() recipeWasSelected: Recipe;
   selectedRecipe: Recipe;
-  constructor() { }
+  // all child will now use the same instance of recipeService
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
+    this.recipeService.recipeSelected.subscribe(
+      (recipe: Recipe) => {
+        this.selectedRecipe = recipe;
+      }
+    )
   }
 
 }
